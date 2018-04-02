7e4f35945d6c7aad1039a1832c84127168fe9166
nataliercraun <saresend@usc.edu>
2018-04-02 03:01:07 -0700

    Added .rb file for the main page feature tests

--- Cucumber/features/step_definitions/main_page_steps.rb
+++ Cucumber/features/step_definitions/main_page_steps.rb
@@ -2,11 +2,20 @@ When(/^I open localhost:(\d+)$/) do |arg1|
 	visit 'localhost:' + arg1
 end
 
-When(/^I open localhost:(\d+)$/) do |arg1|
-	visit 'localhost:' + arg1
+Then(/^I see "([^"]*)" on attribute "([^"]*)" on id "([^"]*)"$/) do |arg1, arg2, arg3|
+  	puts(find(arg3)['id'])
 end
 
-Then(/^I see the name "([^"]*)" on id "([^"]*)" on attribute "([^"]*)"$/) do |arg1, arg2, arg3|
-	expect(find(arg2)['id'] == arg1).to be true 
+When(/^I type "([^"]*)" into "([^"]*)"$/) do |arg1, arg2|
+	find(:css, arg2).set(arg1)
 end
+
+When(/^I hit enter on "([^"]*)"$/) do |arg1|
+	find(:css, arg1).set("\n")
+end
+
+Then(/^I see the attribute "([^"]*)" of id "([^"]*)" is seen on attribute "([^"]*)" on id "([^"]*)"$/) do |arg1, arg2, arg3, arg4|
+	expect(find(:css, arg4).native.css_value(arg3) == find(arg2)['value']).to be true
+end
+
 
