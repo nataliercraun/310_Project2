66876bf66897e8d7b93b55e34abde1fff678279a
Grant Price <gprice@usc.edu>
2018-04-02 00:08:46 -0700

    cucumber filepath fixes / naming convention fixes

--- Cucumber/features/main_page.feature
+++ Cucumber/features/main_page.feature
@@ -1,7 +1,8 @@
 Feature: Main Page 
 
-    Scenario: Main Web Page has a collage topic input box
-       	When I open localhost:8080
+Scenario: Main Web Page has a collage topic input box
+
+	When I open localhost:8080
 	Then I see the name "topicBox" on id ".topicBox" on attribute "id"
 
 
--- Cucumber/features/step_definitions/main_page_steps.rb
+++ Cucumber/features/step_definitions/main_page_steps.rb
@@ -2,6 +2,10 @@ When(/^I open localhost:(\d+)$/) do |arg1|
 	visit 'localhost:' + arg1
 end
 
+When(/^I open localhost:(\d+)$/) do |arg1|
+	visit 'localhost:' + arg1
+end
+
 Then(/^I see the name "([^"]*)" on id "([^"]*)" on attribute "([^"]*)"$/) do |arg1, arg2, arg3|
 	expect(find(arg2)['id'] == arg1).to be true 
 end
